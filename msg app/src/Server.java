53ff9c0f5a14c1e9f3fa5427b4edbda01724fcd0
Bhaswata Sarkar <bhaswatasarkar1@gmail.com>
2022-01-01 19:14:52 +0530

    Second Commit

--- msg app/src/Server.java
+++ msg app/src/Server.java
@@ -1,10 +1,4 @@
-
-
-
-
-
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.*;
 
@@ -17,13 +11,13 @@ public class Server extends JFrame implements ActionListener {
         p1.setBackground(new Color(7,94,84));
         p1.setBounds(0, 0, 450, 70);
         add(p1);
+
+    //"back" icon used for exit
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/3.png")); 
         Image ig1=i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
-        ImageIcon i2=new ImageIcon(ig1);
-        JLabel jl1=new JLabel(i2);
+        i1=new ImageIcon(ig1);
+        JLabel jl1=new JLabel(i1);
         jl1.setBounds(5, 17, 30, 30);
-       
-        
         jl1.addMouseListener(new MouseAdapter()
         {
             public void mouseClicked(MouseEvent ae)
@@ -32,12 +26,24 @@ public class Server extends JFrame implements ActionListener {
             }
         });
         p1.add(jl1);
-        ImageIcon i3 = new ImageIcon(ClassLoader.getSystemResource("icon/1.png"));
-        Image i5 = i3.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
-        ImageIcon i4 = new ImageIcon(i5);
-        JLabel jl2 = new JLabel(i4);
+
+
+
+        ImageIcon i2 = new ImageIcon(ClassLoader.getSystemResource("icon/1.png"));
+        Image ig2 = i2.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
+        i2 = new ImageIcon(ig2);
+        JLabel jl2 = new JLabel(i2);
         jl2.setBounds(40, 5, 60, 60);
         p1.add(jl2);
+
+        JTextField j;
+        j = new JTextField();
+        j.setBounds(100,200,300,400);
+        p1.add(j);
+        
+
+
+
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         setSize(450,700);
